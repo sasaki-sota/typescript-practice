db63938a724b2d81f47d020f1c2833542cf4bca9
soutaschool <55971123+soutaschool@users.noreply.github.com>
2020-06-08 22:16:12 +0900

    fix

---
+++ src/boolean.ts
@@ -0,0 +1,17 @@
+// おまじないとして使用する
+export {};
+
+let name = 'Typescript';
+
+// JSのコード
+// let isFinished = true;
+// console.log({ isFinished });
+
+// TSのコード
+// boolean確としてアノテーションしている
+// アノテーション　＝　変数などに型をつけること
+let isFinished: boolean = true;
+isFinished = false;
+isFinished = 1;
+console.log({ isFinished });
+
